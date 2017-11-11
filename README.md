bc676b1e859964588707a71bf1e5c604d774fc5f
Amine L'ch <aminelch95@gmail.com>
2017-11-11 22:01:51 +0100

    updinting the readme file and adding documentation to the gravatar class

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-gravatarJsApi
+**Manipuler l'API [Gravatar](https://gravatar.com) Gravatar** en **PHP** et **Javascript** via l'**AJAX**.
+
+Ce dépot contient un classe nommé Gravatar.php permet d'intéragier facilement avec Gravatar 
--- php/src/Gravatar.php
+++ php/src/Gravatar.php
@@ -1,4 +1,25 @@
 <?php 
+
+/**
+*  Class Gravatar
+*  Easy way to get a gravatar image 
+*  this class was built from the official documentation of gravatar 
+* Usage:
+* <code>
+*        $email = "youremail@yourhost.com";
+*        $default = "http://www.yourhost.com/default_image.jpg";    // Optional
+*        gravatar::size = 80;
+*        $gravatar::rating = "G";
+*        echo Gravatar::getGravatar();
+* </code>
+*
+*   Class Page: https://github.com/aminelch/gravatarApi/tree/master/php/src
+*
+* @author Amine L'ch <aminelch.github.io>
+* @version 1.0
+* @package Gravatar
+*/
+
 class Gravatar {
 
 public static function getGravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
