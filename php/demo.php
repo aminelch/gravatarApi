5b23af94afd5c73cfcc1296f0be9126ee43ed86a
Amine L'ch <aminelch95@gmail.com>
2017-11-11 21:10:55 +0100

    some modifications

---
+++ php/demo.php
@@ -0,0 +1,23 @@
+<?php 
+require "Gravatar.php"; 
+
+/*
+*        $email = "youremail@yourhost.com";
+*        $default = "http://www.yourhost.com/default_image.jpg";	// Optional
+*        $gravatar = new Gravatar($email, $default);
+*        $gravatar->size = 80;
+*        $gravatar->rating = "G";
+*        $gravatar->border = "FF0000";
+*
+*        echo $gravatar; // Or echo $gravatar->toHTML();
+ */
+
+
+$email = "amine.karismatik@gmail.com";
+$gravatar = new Gravatar($email, null);
+		  
+		  $gravatar->size = 80;
+          $gravatar->rating = "G";
+          $gravatar->border = "FF0000";
+// $gravatar->toHTML();
+echo $gravatar;
